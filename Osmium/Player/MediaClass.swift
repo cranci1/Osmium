6a487ae49895f2fc3ee340f1f9c4b71b894d7e36
cranci1 <trevisanfrancesco022@gmail.com>
2024-05-29 20:49:37 +0200

    improved media player

--- Osmium/Player/MediaClass.swift
+++ Osmium/Player/MediaClass.swift
@@ -9,89 +9,94 @@ import UIKit
 import AVKit
 import MobileCoreServices
 
-class VideoPlayerViewController: UIViewController, UIDocumentPickerDelegate {
-
+class VideoPlayerViewController: UIViewController {
+    
+    @IBOutlet weak var videoView: UIView!
+    
     var player: AVPlayer?
     var playerViewController: AVPlayerViewController?
     var videoURL: URL?
-
+    var pipController: AVPictureInPictureController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func selectVideoButtonPressed(_ sender: UIButton) {
         let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.movie], asCopy: true)
         documentPicker.delegate = self
         documentPicker.modalPresentationStyle = .formSheet
         present(documentPicker, animated: true, completion: nil)
     }
-
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-        guard let url = urls.first else { return }
-        dismiss(animated: true, completion: nil)
-
-        videoURL = url
-        playVideo()
-    }
-
-    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-        dismiss(animated: true, completion: nil)
-    }
-
+    
     func playVideo() {
         guard let videoURL = videoURL else { return }
-
+        
         let asset = AVAsset(url: videoURL)
         let playerItem = AVPlayerItem(asset: asset)
         player = AVPlayer(playerItem: playerItem)
-
+        
         playerViewController = AVPlayerViewController()
         playerViewController?.player = player
-        present(playerViewController!, animated: true) {
-            self.player?.play()
-            self.setupAudioSessionForPlayback()
-            self.enablePiP()
+        
+        if let playerView = playerViewController?.view {
+            playerView.frame = videoView.bounds
+            videoView.addSubview(playerView)
         }
+        
+        player?.play()
+        setupAudioSessionForPlayback()
+        enablePiP()
     }
-
+    
     func setupAudioSessionForPlayback() {
         do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback, options: [])
-            try AVAudioSession.sharedInstance().setActive(true)
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.playback, mode: .moviePlayback, options: [])
+            try audioSession.setActive(true)
         } catch {
             print("Error setting audio session: \(error.localizedDescription)")
         }
     }
-
+    
     func enablePiP() {
-        guard let playerViewController = playerViewController else { return }
-        guard let player = playerViewController.player else { return }
-
-        guard AVPictureInPictureController.isPictureInPictureSupported() else {
+        guard let player = player,
+              AVPictureInPictureController.isPictureInPictureSupported()
+        else {
             print("PiP is not supported on this device.")
-
-            let pipController = AVPictureInPictureController(playerLayer: AVPlayerLayer(player: player))
-            pipController!.delegate = self
-            pipController!.canStartPictureInPictureAutomaticallyFromInline = true
-            pipController!.startPictureInPicture()
-            self.pipController = pipController
-            
             return
         }
+        
+        pipController = AVPictureInPictureController(playerLayer: AVPlayerLayer(player: player))
+        pipController!.delegate = self
+        pipController!.canStartPictureInPictureAutomaticallyFromInline = true
+        pipController!.startPictureInPicture()
     }
+}
 
-    var pipController: AVPictureInPictureController?
+extension VideoPlayerViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else { return }
+        dismiss(animated: true, completion: nil)
+        
+        videoURL = url
+        playVideo()
+    }
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 extension VideoPlayerViewController: AVPictureInPictureControllerDelegate {
     func pictureInPictureControllerDidStartPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
         print("PiP started")
     }
-
+    
     func pictureInPictureControllerWillStopPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
         print("PiP will stop")
     }
-
+    
     func pictureInPictureControllerDidStopPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
         print("PiP stopped")
     }
